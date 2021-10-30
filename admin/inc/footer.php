91f442134ae1f2b25aa9b777a18d192ead38edec
Mahbubur Rahman <mahbubur2310@gmail.com>
2021-10-30 15:20:21 +0600

    Php OOP Ecommerce Website

---
+++ admin/inc/footer.php
@@ -0,0 +1,10 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>&copy; Online Supper shop All rights Reseverd <?php echo date("Y"); ?></p>
+    </div>
+</body>
+</html>
